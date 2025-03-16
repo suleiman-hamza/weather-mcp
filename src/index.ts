839dfe000895e8d751e1a53f04f65790c45d7516
suleiman hamza <hamzasuleiman530@gmail.com>
2025-03-16 12:23:39 +0100

    helper func to query and format data from NWS API

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,44 @@ const server= new McpServer({
     name: "weather",
     version: "1.0.0",
 });
+
+async function makeNWSRequest<T>(url: string): Promise<T | null> {
+    const headers = {
+        "User-Agent": USER_AGENT,
+        Accept: "application/geo+json",
+    };
+
+    try {
+        const response = await fetch(url, {headers});
+        if(!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return (await response.json()) as T;
+    } catch (error) {
+        console.error("Error making NWS request", error)
+        return null;
+    }
+}
+
+interface AlertFeature {
+    properties: {
+      event?: string;
+      areaDesc?: string;
+      severity?: string;
+      status?: string;
+      headline?: string;
+    };
+}
+
+function formatAlert(feature: AlertFeature): string {
+    const props = feature.properties;
+    return [
+        `Event: ${props.event || "Unknown"}`,
+        `Area: ${props.areaDesc || "Unknown"}`,
+        `Severity: ${props.severity || "Unknown"}`,
+        `Status: ${props.status || "Unknown"}`,
+        `Headline: ${props.headline || "No headline"}`,
+        "---",
+    ].join("\n");
+}
